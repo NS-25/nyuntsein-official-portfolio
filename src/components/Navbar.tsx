f6c70af4e3659080c7e91fce8dcd565d471c26ee
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-08-03 23:27:42 -0400

    create a nav bar lists with typscript

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar_logo">Nyunt Sein</div>
+      <ul>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;

7f40f1aba8684147fe11b447537f548241f30e09
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-08-10 12:43:03 -0400

    create AboutMe file for name and bio

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./AboutMe.css";
+
+// Step 1: Define the props shape (TypeScript)
+
+type AboutMeProps = {
+  name: string;
+  bio: string;
+};
+
+const AboutMe: React.FC<AboutMeProps> = ({ name, bio }) => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        <strong>{name}</strong>
+      </p>
+      <p>{bio}</p>
+    </section>
+  );
+};
+export default AboutMe;

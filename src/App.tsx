ea43f580118677e830207947858ace630985f7e9
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-09-09 22:29:19 -0400

    delete all css and navbar details

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
             name="Nyunt Sein"
             bio="I’m a detail-oriented Frontend Developer with over 2 years of hands-on experience building responsive, accessible, and high-performance web applications. Skilled in React, JavaScript, TypeScript, and CSS, I aim to create seamless user experiences and collaborate with teams to deliver impactful products.
 "
-          />
+          />a
         </main>
       </div>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,18 @@ import "./Navbar.css";
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <div className="navbar_logo">Nyunt Sein</div>
-      <ul className="navbar-links">
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+      <div className="navbar_icons">
+        <div>
+          <img src="" className="close_icon" />
+        </div>
+        <div>
+          <img src="" className="subtract_icon" />
+        </div>
+        <div>
+          <img src="" className="full_screen_icon" />
+        </div>
+        <div></div>
+      </div>
     </nav>
   );
 };
